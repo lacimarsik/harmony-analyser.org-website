4e3954587017d5f1243398e5ddc516e86eee7090
lacimarsik <laci.marsik@gmail.com>
2016-09-05 16:09:21 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+	"http://www.w3.org/TR/html4/strict.dtd">
+<html>
+
+<head>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8">
+	<title>Základná stránka webhostingu | Websupport.sk</title>
+	<link rel="shortcut icon" href="http://websupport.sk/images/favicon.ico" type="image/x-icon" />
+	<style type="text/css" media="screen">
+		@import url(http://m.websupport.sk/generic-pages/default/style.css);
+	</style>
+    <?php echo @@file_get_contents('http://sfd.eset.com?format=author')  // eset honeypot ?>
+</head>
+
+<body>
+
+	<div id="header-wrapper">
+		<div id="header">
+			<p>
+				Vitajte na stránke <?php print $_SERVER['SERVER_NAME'] ?>
+			</p>
+			<a href="http://www.websupport.sk" title="Websupport - domeny, webhosting, servery" class="logo">Hosting WebSupport</a>
+		</div>
+	</div>
+	
+	<div id="main">
+		<h1>
+			Nič tu nie je, zatiaľ...
+		</h1>
+		<h2>
+			Toto je základná stránka pre <a href="http://websupport.sk/" title="Websupport - webhosting, domény, server">WebSupport webhosting</a>, môžete ju zmazať a na jej miesto nahrať svoj web.<br />
+			
+		</h2>
+	</div>
+
+</body>
+
+</html>
