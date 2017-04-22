2207298842f67599bd5ef0cb1350999bbf7913a8
lacimarsik <laci.marsik@gmail.com>
2017-04-22 15:38:42 +0200

    Fix JS

--- javascript.js
+++ javascript.js
@@ -11,13 +11,13 @@ function show() {
 }
 
 function hidestandalone() {
-    document.getElementById('previous-releases').style.display='none';
-    document.getElementById('show').style.display='block';
-    document.getElementById('hide').style.display='none';
+    document.getElementById('previous-releases-standalone').style.display='none';
+    document.getElementById('showstandalone').style.display='block';
+    document.getElementById('hidestandalone').style.display='none';
 }
 
 function showstandalone() {
-    document.getElementById('previous-releases').style.display='block';
-    document.getElementById('show').style.display='none';
-    document.getElementById('hide').style.display='block';
+    document.getElementById('previous-releases-standalone').style.display='block';
+    document.getElementById('showstandalone').style.display='none';
+    document.getElementById('hidestandalone').style.display='block';
 }
