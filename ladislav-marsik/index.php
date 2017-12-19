4b616ce81ef70ff4e904522698e7f2d7a3da7987
lacimarsik <laci.marsik@gmail.com>
2017-12-19 14:33:30 +0100

    Add Google Scholar

--- ladislav-marsik/index.php
+++ ladislav-marsik/index.php
@@ -126,6 +126,7 @@
 			<a class="profile" href="http://www.researcherid.com/rid/G-9018-2017">ResearcherID</a>
 			<a class="profile" href="https://www.scopus.com/authid/detail.uri?authorId=57194063739">Scopus</a>
 			<a class="profile" href="https://www.researchgate.net/profile/Ladislav_Marsik">ResearchGate</a>
+			<a class="profile" href="https://scholar.google.cz/citations?user=z5TXAsEAAAAJ">Google Scholar</a>
 			<a class="profile" href="https://dl.acm.org/author_page.cfm?id=99659174894">ACM DL</a>
 			<a class="profile" href="http://dblp.uni-trier.de/pers/hd/m/Marsik:Ladislav">DBLP</a>
 			<a class="profile" href="https://github.com/lacimarsik">GitHub</a>
